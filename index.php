ea0c836779258cadada8a5aefbd71da31d1b9781
Gubertas <gubertas.maminskas>
2019-10-29 10:40:07 +0200

    code

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <html>
-<head>
-<title>Naga-No
-</title> 
-<article>
-  <header>
-    <h1>Gubertas html asas</h1>
-  </header>
-  <p>Moku kurti statini turini.</p>
-</article>
+    <head>
+        <title>Naga-No</title> 
+    </head>
+    <body>
+        <h1>Gubertas html asas</h1>
+        <p>Moku kurti statini turini.</p> 
+    </body>
 </html>
