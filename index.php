efc329fe64ee796f437221118830e98f0a38c6f4
Gubertas <gubertas.maminskas>
2019-10-29 11:54:53 +0200

    DATE

--- index.php
+++ index.php
@@ -1,17 +1,15 @@
 
 <html>
     <head>
-        <title>Naga-No <?php print 'ir PHP ';
+        <title>Naga-No, <?php print date('d') ?> <?php print 'ir PHP ';
 ?> </title> 
     </head>
     <body>
         <h1><strong>Gubertas</strong> - HTML <?php print 'ir PHP ';
-?> asas <?php
+?> asas jau nuo <?php print date('y') ?> metu <?php
 ?> </h1>
-        <p>Moku kurti statini turini. atidaryta. <?php
-            print 'Laikas ';
-            date('Y-m-d H:i:s');
-            print date('Y-m-d H:i:s');
-            ?></p> 
+        <p>Viskas prasidejo <?php print date('m') ?> menesio,<?php print date('d') ?> diena <br>
+             <?php print date('Y-m-d H:i:s'); ?>
+        </p> 
     </body>
 </html>
