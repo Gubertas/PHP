004911ea3b33b2f691413064acacd5ce410f4e80
Gubertas <gubertas.maminskas>
2019-10-29 11:08:51 +0200

    nebus eroror

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
+
 <html>
     <head>
-        <title>Naga-No</title> 
+        <title>Naga-No <?php print 'ir PHP ';
+?> </title> 
     </head>
     <body>
-        <h1>Gubertas html asas</h1>
-        <p>Moku kurti statini turini.</p> 
+        <h1><strong>Gubertas</strong> - HTML <?php print 'ir PHP ';
+?> asas <?php
+?> </h1>
+        <p>Moku kurti statini turini. atidaryta. <?php
+            print 'Laikas ';
+            date('Y-m-d H:i:s');
+            print date('Y-m-d H:i:s');
+            ?></p> 
     </body>
 </html>
