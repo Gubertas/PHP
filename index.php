1684af559bd3b54676596f4f8ff7dda58b5ae2e5
Gubertas <gubertas.maminskas>
2019-10-29 10:37:08 +0200

    MESSAGE

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
-<?php
-print "veikia";
+<html>
+<head>
+<title>Naga-No
+</title> 
+<article>
+  <header>
+    <h1>Gubertas html asas</h1>
+  </header>
+  <p>Moku kurti statini turini.</p>
+</article>
+</html>
