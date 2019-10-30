36f9a8868689b1a00e8dea0c5ed9a17f524b2c94
Gubertas <gubertas.maminskas>
2019-10-30 12:55:40 +0200

    bomba

--- index.php
+++ index.php
@@ -1,29 +1,36 @@
 <html>
-    <!-- <body style="background-color:rgb;" 
-    </font>naGa mano nick <?php print date('Y-m-d', strtotime('+' . rand(2, 10) . 'year')); ?>> -->
+
     <head>
-        <title>PHP lydės ir <?php print date('Y-m-d', strtotime('+' . rand(1, 5) . 'years')); ?></title>
+        <title> NAGA <?php print date('Y-m-d', strtotime('+' . rand(1, 5) . 'years')); ?></title>
         <style>
-
-        body{
-            background-color: rgb(<?php print rand(0,255) . "," . rand(0,255) . "," . rand(0,255); ?>);
+            .center {
+                display:flex;
+                justify-content: center;
+                align-items: center;
+                flex-direction: column;
             }
-            h1{
-               font-size: <?php print rand(4,24); ?>; px;
+            .bomb {
+                background-size: <?php print date('s'); ?>%;
+                background-repeat: no-repeat;
+                height: 500px;
+                width: 500px;
+                background-image: url('assets/images/bomb-clipart-cherrybomb.png');
             }
-            
+            .bomb-00 {
+                background-size: contain;
+                background-image: url('assets/images/HALLOWEEN.gif');
+            </style>
+        </head>
+        <body class="center">
+            <div class="bomb bomb-<?php print date('s'); ?>"></div>
+            <p><?php print date('s'); ?></p>
+        </body>
 
 
-    </style>
-    </head>
+    </html>
 
-    
-    <body>  
 
-        <h1><b>naGa</b> - Noriu buti OG su <?php print rand(1, 5); ?> Ginklais</h1>   
-        <!-- savo dydis pakeistas -->
-        <p><font color="rgba"<?php print rand(1, 5); ?> Ginklais</font>naGa mano nick <?php print date('Y-m-d', strtotime('+' . rand(2, 10) . 'year')); ?> > - Noriu buti OG su </p>
-        <!-- savo dydis pakeistas -->
 
-    </body>
-</html>  
+
+
+
