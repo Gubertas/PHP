fdd5f934f95918e05c0b74148a03c2ff56dcbb85
Gubertas <gubertas.maminskas>
2019-10-30 10:54:50 +0200

    dydisRGB

--- index.php
+++ index.php
@@ -1,11 +1,29 @@
 <html>
+    <!-- <body style="background-color:rgb;" 
+    </font>naGa mano nick <?php print date('Y-m-d', strtotime('+' . rand(2, 10) . 'year')); ?>> -->
     <head>
         <title>PHP lydės ir <?php print date('Y-m-d', strtotime('+' . rand(1, 5) . 'years')); ?></title>
+        <style>
+
+        body{
+            background-color: rgb(<?php print rand(0,255) . "," . rand(0,255) . "," . rand(0,255); ?>);
+            }
+            h1{
+               font-size: <?php print rand(4,24); ?>; px;
+            }
+            
+
+
+    </style>
     </head>
-    <body>
-        <h1><b>naGa</b> <font size="3" >- Noriu buti OG su <?php print rand(1, 5); ?> Ginklais</font> </h1>   
+
+    
+    <body>  
+
+        <h1><b>naGa</b> - Noriu buti OG su <?php print rand(1, 5); ?> Ginklais</h1>   
         <!-- savo dydis pakeistas -->
-        <p><font color="red"<?php print rand(1, 5); ?> Ginklais</font>naGa mano nick <?php print date('Y-m-d', strtotime('+' . rand(2, 10) . 'year')); ?> > - Noriu buti OG su </p>
+        <p><font color="rgba"<?php print rand(1, 5); ?> Ginklais</font>naGa mano nick <?php print date('Y-m-d', strtotime('+' . rand(2, 10) . 'year')); ?> > - Noriu buti OG su </p>
         <!-- savo dydis pakeistas -->
+
     </body>
 </html>  
