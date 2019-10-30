978d482e7fbd2ecf32afabfafab17148459a873f
Gubertas <gubertas.maminskas>
2019-10-30 10:09:06 +0200

    SPALVAirDydis

--- index.php
+++ index.php
@@ -3,7 +3,9 @@
         <title>PHP lydės ir <?php print date('Y-m-d', strtotime('+' . rand(1, 5) . 'years')); ?></title>
     </head>
     <body>
-        <h1><b>naGa</b> - galbūt turėsiu <?php print rand(1, 5); ?> vaikus</h1>
-        <p>D. Trump'as nebebus prezidentu: <?php print date('Y-m-d', strtotime('+' . rand(2, 10) . 'year')); ?></p>
+        <h1><b>naGa</b> <font size="3" >- Noriu buti OG su <?php print rand(1, 5); ?> Ginklais</font> </h1>   
+        <!-- savo dydis pakeistas -->
+        <p><font color="red"<?php print rand(1, 5); ?> Ginklais</font>naGa mano nick <?php print date('Y-m-d', strtotime('+' . rand(2, 10) . 'year')); ?> > - Noriu buti OG su </p>
+        <!-- savo dydis pakeistas -->
     </body>
 </html>  
