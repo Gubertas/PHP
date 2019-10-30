730770efca3c124b7af3d25524e02e26687c3a52
Gubertas <gubertas.maminskas>
2019-10-30 09:59:45 +0200

    saule

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
-
 <html>
     <head>
-        <title>Komentarai </title> 
+        <title>PHP lydės ir <?php print date('Y-m-d', strtotime('+' . rand(1, 5) . 'years')); ?></title>
     </head>
     <body>
-        <h1><strong>Gubertas</strong> dainu tekstai   </h1>
-        <h2> paragrafo daina   </h2>
-        <p>as toks vienisas jaunas paragrafas</p> 
-      <!--  <p>mindau svetimo kodo gatves</p> -->
+        <h1><b>naGa</b> - galbūt turėsiu <?php print rand(1, 5); ?> vaikus</h1>
+        <p>D. Trump'as nebebus prezidentu: <?php print date('Y-m-d', strtotime('+' . rand(2, 10) . 'year')); ?></p>
     </body>
-</html>
+</html>  
