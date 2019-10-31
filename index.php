af3cb25997a92fdf9af0b811987fa8d498476c4a
Gubertas <gubertas.maminskas>
2019-10-31 10:58:48 +0200

    fuck

--- index.php
+++ index.php
@@ -1,36 +1,20 @@
+<?php
+$img_src = 'assets/images/HALLOWEEN.gif';
+;
+?>
 <html>
 
     <head>
         <title> NAGA <?php print date('Y-m-d', strtotime('+' . rand(1, 5) . 'years')); ?></title>
-        <style>
-            .center {
-                display:flex;
-                justify-content: center;
-                align-items: center;
-                flex-direction: column;
-            }
-            .bomb {
-                background-size: <?php print date('s'); ?>%;
-                background-repeat: no-repeat;
-                height: 500px;
-                width: 500px;
-                background-image: url('assets/images/bomb-clipart-cherrybomb.png');
-            }
-            .bomb-00 {
-                background-size: contain;
-                background-image: url('assets/images/HALLOWEEN.gif');
-            </style>
-        </head>
-        <body class="center">
-            <div class="bomb bomb-<?php print date('s'); ?>"></div>
-            <p><?php print date('s'); ?></p>
-        </body>
 
+    </head>
+    <body>
+        <img src="<?php print $img_src ?>">
+        <img src="<?php print $img_src ?>">
+        <img src="<?php print $img_src ?>">
+        <img src="<?php print $img_src ?>">
+             </body> 
 
-    </html>
-
-
-
-
+             </html>
 
 
